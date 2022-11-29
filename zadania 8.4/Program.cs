7a36dbfea3220ed25a1d96d23f4f7826f9c93552
xDusz3k <michal124dyduch@gmail.com>
2022-11-29 20:45:59 +0100

    Zrobione do 8.4.6

--- zadania 8.4/Program.cs
+++ zadania 8.4/Program.cs
@@ -1,200 +1,184 @@
 ﻿using System;
+using System.Data;
 
 namespace zadania_8._4
 {
     internal class Program
     {
         /* wyswietlanie tablicy */
-        static void printTable(int[] table)
-        {
-            string txt = "[";
-            for (int i = 0; i < table.Length; i++)
-            {
-                if (table[i] == 0)
-                {
-                    break;
-                }
-                txt += table[i];
-                if (i < table.Length - 1)
-                {
-                    txt += ", ";
-                }
-            }
-            txt += "]";
-            Console.WriteLine(txt);
-        }
+        //static void printTable(int[] table)
+        //{
+        //    string txt = "[";
+        //    for (int i = 0; i < table.Length; i++)
+        //    {
+        //        if (table[i] == 0)
+        //        {
+        //            break;
+        //        }
+        //        txt += table[i];
+        //        if (i < table.Length - 1)
+        //        {
+        //            txt += ", ";
+        //        }
+        //    }
+        //    txt += "]";
+        //    Console.WriteLine(txt);
+        //}
 
         static void Main(string[] args)
         {
             Console.WriteLine("Podaj długość ciągu");
             int n = int.Parse(Console.ReadLine());
-            string Parzyste = "";
-            string NieParzyste = "";
-            string PodzielnePrzezTrzy = "";
-            string Przedzial = "";
-            for (int i = 0; i <= n; i++)
+            int[] tablica = new int[n];
+            for (int i = 0; i < n; i++)
             {
-                if (i % 2 == 0)
+                Console.WriteLine("Podaj liczbe");
+                int temp = int.Parse(Console.ReadLine());
+                tablica[i] = temp;
+            }
+            int[] Parzyste = new int[n];
+            int liczbaParzystych = 0;
+            int[] NieParzyste = new int[n];
+            int liczbaNieParzystych = 0;
+            int[] PodzielnePrzezTrzy = new int[n];
+            int liczbaPodzielnychPrzezTrzy = 0;
+            int[] Przedzial = new int[n];
+            int liczbaPrzedzial = 0;
+            int[] Cyfry = new int[n];
+            int liczbaCyfr = 0;
+            int[] SumaCyfr = new int[n];
+            int liczbaSumyCyfr = 0;
+            int[] DodanieSto = new int[n];
+            int liczbaSto = 0;
+            int[] Ujemne = new int[n];
+            int liczbaUjemne = 0;
+
+
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                if (tablica[i] % 2 == 0)
                 {
-                    Parzyste += i  + ", ";
-                   
+                    Parzyste[liczbaParzystych] = tablica[i];
+                    liczbaParzystych++;
                 }
                 else
                 {
-                    NieParzyste += i + ", ";
+                    NieParzyste[liczbaNieParzystych] = tablica[i];
+                    liczbaNieParzystych++;
                 }
-                if (i % 3 == 0)
+                if (tablica[i] % 3 == 0)
                 {
-                    PodzielnePrzezTrzy += i + ", ";              
+                    PodzielnePrzezTrzy[liczbaPodzielnychPrzezTrzy] = tablica[i];
+                    liczbaPodzielnychPrzezTrzy++;
                 }
-                if (i >= 4 && i <15)
+                if (tablica[i] >= 4 && tablica[i] < 15)
                 {
-                    Przedzial += i + ", ";
+                    Przedzial[liczbaPrzedzial] = tablica[i];
+                    liczbaPrzedzial++;
                 }
+                int a = tablica[i];
+                while (a != 0)
+                {
+                    int b = a % 10;
+                    if (b % 2 != 0)
+                    {
+                        break;
+                    }
+                    a /= 10;
+                    if (a == 0)
+                    {
+                        Cyfry[liczbaCyfr] = tablica[i];
+                        liczbaCyfr++;
+                    }
+                }
+                a = tablica[i];
+                int Suma = 0;
+                while (a > 0)
+                {
+                    Suma += a % 10;
+                    a /= 10;
+                }
+                if (Suma == 1)
+                {
+                    SumaCyfr[liczbaSumyCyfr] = tablica[i];
+                    liczbaSumyCyfr++;
+                }
+                if (tablica[i] % 2 == 0)
+                {
+                    DodanieSto[liczbaSto] = tablica[i] + 100;
+                    liczbaSto++;
+                }
+                if (tablica[i] < 0)
+                {
+                    tablica[i] = 0;
+                    Ujemne[liczbaUjemne] = tablica[i];
+                    liczbaUjemne++;
+                }
+                else
+                {
+                    Ujemne[liczbaUjemne] = tablica[i];
+                    liczbaUjemne++;
+                }
+
+
             }
-            Console.WriteLine("Liczby parzyste:");
-            Console.WriteLine(Parzyste);
-            Console.WriteLine("Liczby nieparzyste:");
-            Console.WriteLine(NieParzyste);
-            Console.WriteLine("Liczby, które dzielą się przez 3");
-            Console.WriteLine(PodzielnePrzezTrzy);
+
+
+            Console.WriteLine("Liczby parzyste");
+            for (int i = 0; i < liczbaParzystych; i++)
+            {
+                Console.Write($"{Parzyste[i]}, ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Liczby nieparzyste");
+            for (int i = 0; i < liczbaNieParzystych; i++)
+            {
+                Console.Write($"{NieParzyste[i]}, ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Liczby podzielne przez 3");
+            for (int i = 0; i < liczbaPodzielnychPrzezTrzy; i++)
+            {
+                Console.Write($"{PodzielnePrzezTrzy[i]}, ");
+            }
+
+            Console.WriteLine();
             Console.WriteLine("Liczby z przedziału [4;15)");
-            Console.WriteLine(Przedzial);
+            for (int i = 0; i < liczbaPrzedzial; i++)
+            {
+                Console.Write($"{Przedzial[i]}, ");
+            }
 
+            Console.WriteLine();
+            Console.WriteLine("Liczby, ktore mają wszystkie cyfry parzyste");
+            for (int i = 0; i < liczbaCyfr; i++)
+            {
+                Console.Write($"{Cyfry[i]}, ");
+            }
 
+            Console.WriteLine();
+            Console.WriteLine("Liczby, których suma cyfr wynosi 1");
+            for (int i = 0; i < liczbaSumyCyfr; i++)
+            {
+                Console.Write($"{SumaCyfr[i]}, ");
+            }
 
+            Console.WriteLine();
+            Console.WriteLine("Liczby po dodaniu do nich 100");
+            for (int i = 0; i < liczbaSto; i++)
+            {
+                Console.Write($"{DodanieSto[i]}, ");
+            }
 
-
-
-
-
-
-
-
-
-            //Console.WriteLine("Podaj długość ciągu");
-            //int n = int.Parse(Console.ReadLine());
-            //int[] LiczbyParzyste = new int[n];
-            //int[] LiczbyNieParzyste = new int[n];
-            //int[] LiczbyPodzielnePrzezTrzy = new int[n];
-            //int[] LiczbyZPrzedzialu = new int[n];
-            //int[] CyfryLiczbParzyste = new int[n];
-            //int[] SumaCyfrRownaJeden = new int[n];
-            //for (int i = 0; i <= n; i++)
-            //{
-            //    string liczba = Convert.ToString(i);
-            //    bool CyfryParzyste = true;
-            //    int SumaCyfr = 0;
-
-            //    for (int ii = 0; ii < liczba.Length; ii++)
-            //    {
-            //        //int Cyfra = (int)liczba[ii];
-            //        SumaCyfr = SumaCyfr + (int)liczba[ii]-48;
-
-            //        if (Convert.ToInt64(liczba[ii]) % 2 != 0)
-            //        {
-            //            CyfryParzyste = false;
-            //        }
-            //    }
-            //    Console.WriteLine(SumaCyfr);
-            //    if (CyfryParzyste)
-            //    {
-            //        for (int ii = 0; ii < CyfryLiczbParzyste.Length; ii++)
-            //        {
-            //            if (CyfryLiczbParzyste[ii] == 0)
-            //            {
-            //                CyfryLiczbParzyste[ii] = i;
-            //                break;
-            //            }
-            //        }
-            //    }
-
-
-            //    for (int ii = 0; ii < liczba.Length; ii++)
-            //    {
-            //        int Cyfra = liczba[ii];
-            //        SumaCyfr = SumaCyfr + (int)Cyfra;
-            //        Console.WriteLine($"\n\n{Cyfra} \n\n") ;
-            //    }
-            //    if (SumaCyfr == 1)
-            //    {
-            //        for (int ii = 0; ii < SumaCyfrRownaJeden.Length; ii++)
-            //        {
-            //            if (SumaCyfrRownaJeden[ii] == 0)
-            //            {
-            //                SumaCyfrRownaJeden[ii] = i;
-            //                break;
-            //            }
-            //        }
-            //    }
-            //    if (i % 2 == 0)
-            //    {
-            //        //Console.WriteLine($"Liczba {i} jest parzysta");
-            //        //Console.ForegroundColor = ConsoleColor.Green;
-            //        for (int ii = 0; ii < LiczbyParzyste.Length; ii++)
-            //        {
-            //            if (LiczbyParzyste[ii] == 0)
-            //            {
-            //                LiczbyParzyste[ii] = i;
-            //                break;
-            //            }
-            //        }
-            //    }
-            //    else if (i % 2 == 1)
-            //    {
-            //        //Console.WriteLine($"Liczba {i} jest nieparzysta");
-            //        //Console.ForegroundColor = ConsoleColor.Red;
-            //        for (int ii = 0; ii < LiczbyNieParzyste.Length; ii++)
-            //        {
-            //            if (LiczbyNieParzyste[ii] == 0)
-            //            {
-            //                LiczbyNieParzyste[ii] = i;
-            //                break;
-            //            }
-            //        }
-            //    }
-            //    if (i % 3 == 0)
-            //    {
-            //        //Console.WriteLine($"Liczba {i} dzieli się przez 3");
-            //        for (int ii = 0; ii < LiczbyPodzielnePrzezTrzy.Length; ii++)
-            //        {
-            //            if (LiczbyPodzielnePrzezTrzy[ii] == 0)
-            //            {
-            //                LiczbyPodzielnePrzezTrzy[ii] = i;
-            //                break;
-            //            }
-            //        }
-            //    }
-            //    //else
-            //    //{
-            //    //    Console.WriteLine($"Liczba {i} nie dzieli się przez 3");
-            //    //}
-            //    if (i >= 4 && i <= 15)
-            //    {
-            //        for (int ii = 0; ii < LiczbyZPrzedzialu.Length; ii++)
-            //        {
-            //            if (LiczbyZPrzedzialu[ii] == 0)
-            //            {
-            //                LiczbyZPrzedzialu[ii] = i;
-            //                break;
-            //            }
-            //        }
-            //    }
-            //}
-            //Console.WriteLine("Liczby parzyste");
-            //Console.ForegroundColor = ConsoleColor.Red;
-            //printTable(LiczbyParzyste);
-            //Console.WriteLine("Liczby nie parzyste");
-            //Console.ForegroundColor = ConsoleColor.Blue;
-            //printTable(LiczbyNieParzyste);
-            //Console.WriteLine("Liczby podzielne przez 3");
-            //printTable(LiczbyPodzielnePrzezTrzy);
-            //Console.WriteLine("Liczby z przedziału [4,15]");
-            //printTable(LiczbyZPrzedzialu);
-            //Console.WriteLine("Liczby, których cyfry są parzyste");
-            //printTable(CyfryLiczbParzyste);
-            //Console.WriteLine("Liczby, których cyfry są równe 1");
-            //printTable(SumaCyfrRownaJeden);
+            Console.WriteLine();
+            Console.WriteLine("Ciąg liczb, w którym liczby ujemne zamieniane są na 0");
+            for (int i = 0; i < liczbaUjemne; i++)
+            {
+                Console.Write($"{Ujemne[i]}, ");
+            }
         }
     }
 }
+
+
